83a9104d3ca35d8ea7fe8b653668e1f86fc2e4dc
jswift <jswift@thacher.org>
2015-04-30 21:27:53 -0700

    figured out scaling issue with the sky brightness calibration.

--- sky_brightness_batch.py
+++ sky_brightness_batch.py
@@ -9,18 +9,27 @@ from astropy import wcs
 from astropy.io.fits import open
 import robust as rb
 from djs_photfrac_mb import *
+import sys
+from scipy.signal import convolve2d
+from scipy.ndimage import gaussian_filter 
 
+do_smooth = False
+
+# Specifics of the Thacher Observatory
 lat = 34.467028
 lon = -119.1773417
 deczen = lat
 
+# Create pyephem observatory object 
 thob = ephem.Observer()
 thob.long = ephem.degrees("-119.1773417")
 thob.lat = ephem.degrees("34.467028")
 thob.elevation = 504.4 
 
+# Data directory
 dir = '/Users/jonswift/Dropbox (Thacher)/Observatory/AllSkyCam/Data/13April2015/'
-file = 'Image_15_crop2_astrom.fits'
+# Image with astrometric solution from astrometry.net
+file = 'Image_15_crop4_astrom.fits'
 
 rate = 0.1500
 darkrate = 0.0157 # from D. McKenna
@@ -54,19 +63,28 @@ radeg  = np.degrees(ra)
 decdeg = np.degrees(dec)
 xpix,ypix = w.wcs_world2pix(radeg,decdeg,1) # Pixel coordinates of (RA, DEC)
 
+
+
+# Get indices of 5 degree field
 pixsz  = np.sqrt(header['CD1_1']**2 + header['CD1_2']**2)
 radpix = 2.5/pixsz 
-
 ap = djs_photfrac(ypix,xpix,radpix,xdimen=xsz,ydimen=ysz)
 
+# Smooth image
+sigma = 2*radpix/2.355
+if do_smooth:
+    smooth = gaussian_filter(image,sigma)
+else:
+    smooth = image
+    
 # Image characteristics and plot
-sig = rb.std(image)
-med = np.median(image)
+sig = rb.std(smooth)
+med = np.median(smooth)
 vmin = med - 3*sig
 vmax = med + 5*sig
 plt.figure(1)
 plt.clf()
-plt.imshow(image,vmin=vmin,vmax=vmax,cmap='gist_heat',interpolation='nearest', \
+plt.imshow(smooth,vmin=vmin,vmax=vmax,cmap='gist_heat',interpolation='nearest', \
            origin='lower')
 plt.scatter(xpix,ypix,marker='+',s=100,facecolor='none',edgecolor='yellow', \
             linewidth=1.5)
@@ -76,8 +94,8 @@ plt.axis('off')
 plt.title('Field Center')
 plt.savefig('Center.png',dpi=300)
 
-image2 = image*0
-image2[ap['xpixnum'],ap['ypixnum']] = image[ap['xpixnum'],ap['ypixnum']]
+image2 = smooth*0
+image2[ap['xpixnum'],ap['ypixnum']] = smooth[ap['xpixnum'],ap['ypixnum']]
 
 plt.figure(2)
 plt.clf()
@@ -91,14 +109,15 @@ plt.axis('off')
 plt.title('Photometer Field of View')
 plt.savefig('FOV.png',dpi=300)
 
-ftot = np.sum(image[ap['xpixnum'],ap['ypixnum']])
-npix = np.sum(ap['fracs'])
+# Get median value in apeture
+#ftot = np.sum(image2[ap['xpixnum'],ap['ypixnum']])
+#npix = np.sum(ap['fracs'])
+#avgval = ftot/npix
+avgval = np.median(image2[ap['xpixnum'],ap['ypixnum']])
 
-avgval = ftot/npix
-# Get rid of stars
-avgval = np.median(image[ap['xpixnum'],ap['ypixnum']])
-avgval = np.min(image[ap['xpixnum'],ap['ypixnum']])
 
+#avgval = np.median(image[ap['xpixnum'],ap['ypixnum']])
+#avgval = np.min(image[ap['xpixnum'],ap['ypixnum']])
 
 logavg = -2.5*np.log10(avgval)
 calval = 21.029 - logavg
@@ -106,7 +125,21 @@ fullfile = 'img00015_ds.fit'
 
 # Get image and header
 fullim, fullh = pf.getdata(dir+fullfile, 0, header=True)
-logim = -2.5*np.log10(fullim)
+
+# Smooth full image
+if do_smooth:
+    fsmooth = gaussian_filter(fullim,sigma)
+else:
+    fsmooth = fullim
+    
+# Select out horizon
+#if do_smooth:
+inds = np.where(fullim <= 500)
+fullim[inds] = 1e-5
+fsmooth[inds] = fullim[inds]
+
+finalim = fsmooth
+logim = -2.5*np.log10(finalim)
 calim = logim + calval
 
 ysz, xsz = np.shape(calim)
@@ -114,22 +147,22 @@ calap = djs_photfrac(ysz/2,xsz/2+40,ysz/2.0,xdimen=ysz,ydimen=xsz)
 newcal = calim*np.inf
 newcal[calap['xpixnum'],calap['ypixnum']] = calim[calap['xpixnum'],calap['ypixnum']]
 cmap='CMRmap_r'
-vmin = 20
-vmax = 22.5
+vmin = 19.0
+vmax = 22.0
 plt.figure(3)
 plt.clf()
 plt.imshow(newcal,vmin=vmin,vmax=vmax,cmap=cmap,interpolation='nearest', \
            origin='upper')
 plt.axis('off')
-plt.colorbar(shrink=0.65,aspect=10,ticks=[20,20.5,21,21.5,22], \
+plt.colorbar(shrink=0.65,aspect=10,ticks=[19,19.5,20,20.5,21,21.5,22], \
     orientation='horizontal',pad=0.075)
-plt.annotate('N',[0.52,0.91],horizontalalignment='center',xycoords='figure fraction',\
+plt.annotate('N',[0.53,0.91],horizontalalignment='center',xycoords='figure fraction',\
     fontsize=18)
-plt.annotate('S',[0.52,0.23],horizontalalignment='center',xycoords='figure fraction',\
+plt.annotate('S',[0.53,0.24],horizontalalignment='center',xycoords='figure fraction',\
     fontsize=18)
-plt.annotate('E',[0.25,0.58],horizontalalignment='center',xycoords='figure fraction',\
+plt.annotate('E',[0.28,0.58],horizontalalignment='center',xycoords='figure fraction',\
     fontsize=18)
-plt.annotate('W',[0.795,0.58],horizontalalignment='center',xycoords='figure fraction',\
+plt.annotate('W',[0.785,0.58],horizontalalignment='center',xycoords='figure fraction',\
     fontsize=18)
 plt.annotate(r'mags/arcsec$^2$',[0.5,0.07],horizontalalignment='center', \
              xycoords='figure fraction',fontsize=12)
